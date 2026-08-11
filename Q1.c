0d1481d74c81f373c8a127bfa2bfed081b4366fe
sololeveler8771-ui <solo.leveler.8771@gmail.com>
2026-08-12 00:56:04 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    int b;
+    printf("enter 1st number - ");
+    scanf("%d", &a);
+    printf("enter 2nd number - ");
+    scanf("%d", &b);
+    int sum = a + b;
+    printf("The sum is - %d", sum);
+}
