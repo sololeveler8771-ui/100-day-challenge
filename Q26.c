9441126fed6cb74f522a2696c2ef023be84a2c43
sololeveler8771-ui <solo.leveler.8771@gmail.com>
2026-09-07 02:43:58 +0530

    Add files via upload

---
+++ Q26.c
@@ -0,0 +1,32 @@
+/*Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+Sample Test Cases:
+Input 1:
+4 2 +
+Output 1:
+6
+
+Input 2:
+10 3 %
+Output 2:
+1
+
+Input 3:
+15 5 /
+Output 3:
+3
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a no. : ");
+    scanf("%d", &n);
+    for(int i; i<=n; i++)
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+}
